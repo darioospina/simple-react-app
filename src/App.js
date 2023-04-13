7baf618ce064d73f58bc16bb6681e369254f2358
darioospina <65924368+darioospina@users.noreply.github.com>
2023-04-13 12:06:08 -0600

    First commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import foto from './foto.png'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={foto} id='myphoto' alt='My photo' width={'200px'}/>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is my first React App deployed on AWS
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Visit me at <a href='https://www.dario-ospina.com' style={{color: 'yellow'}}>dario-ospina.com</a></p>
+
       </header>
     </div>
   );
